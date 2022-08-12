13c87da7d5fe53a561fb59c79066f3a0344caf6c
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-12 21:15:36 +0300

    Step 1 - 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,104 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 80,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+
+
+
+
+export class App extends Component {
+  
+  static defaultProps = {
+    initialGood: 0,
+    initialNeutral: 0,
+    initialBad: 0,
+  };
+
+  static propTypes = {
+    //
+  };
+
+  state = {
+    good: this.props.initialGood,
+    neutral: this.props.initialNeutral,
+    bad: this.props.initialBad,
+  };
+
+  goodIncrement = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  neutralIncrement = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  badIncrement = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+
+    return (
+        
+        <div className="FeedBack">
+          <h1>Please leave feedback</h1>
+
+          <button type="button" onClick={this.goodIncrement}>Good</button>
+          <button type="button" onClick={this.neutralIncrement}>Neutral</button>
+          <button type="button" onClick={this.badIncrement}>Bad</button>
+
+          <h1>Statistics</h1>
+
+          <p>Good {good}</p>
+          <p>Neutral {neutral}</p>
+          <p>Bad {bad}</p>
+          <p>Total: {totalTodoCount}</p>
+          <p>Positive feedback: {completedTodoCount}</p>
+        </div>
+        
+
+        
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//! OLD ========================================================
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 80,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
