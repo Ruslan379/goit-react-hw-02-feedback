3a7065412a46c939346356b3b514fb808691f44f
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-12 21:48:54 +0300

    Step 2- 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,16 @@ import React, { Component } from 'react';
 
 export class App extends Component {
   
+ 
+
   static defaultProps = {
     initialGood: 0,
     initialNeutral: 0,
     initialBad: 0,
+
+    initialTotalFeedback: 0,
+    initialPositiveFeedbackPercentage: 0,
+
   };
 
   static propTypes = {
@@ -21,6 +27,12 @@ export class App extends Component {
     neutral: this.props.initialNeutral,
     bad: this.props.initialBad,
   };
+
+  feedBack = {
+    totalFeedback: this.props.initialTotalFeedback,
+    positiveFeedbackPercentage: this.props.initialPositiveFeedbackPercentage,
+  }
+
 
   goodIncrement = () => {
     this.setState(prevState => ({
@@ -41,14 +53,15 @@ export class App extends Component {
   };
 
   countTotalFeedback = () => {
-    this.setState(prevState => ({
-      bad: prevState.bad + 1,
-    }));
+    this.setState(prevState => (
+      this.totalFeedback = prevState.good + prevState.neutral + prevState.bad
+      
+    ));
   };
 
   render() {
     const { good, neutral, bad } = this.state;
-
+    console.log(this.totalFeedback);
 
     return (
         
@@ -64,8 +77,9 @@ export class App extends Component {
           <p>Good {good}</p>
           <p>Neutral {neutral}</p>
           <p>Bad {bad}</p>
-          <p>Total: {totalTodoCount}</p>
-          <p>Positive feedback: {completedTodoCount}</p>
+        
+          <p>Total: {this.countTotalFeedback}</p>
+          {/* <p>Positive feedback: {completedTodoCount}</p> */}
         </div>
         
 
