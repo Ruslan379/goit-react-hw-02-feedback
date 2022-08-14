cc4f453020a6253568d25b13e2de42223f85168c
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-14 18:59:16 +0300

    Work 100% - 18 !!! (work on styles)

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CapitalizeFirstLetter } from 'components/CapitalizeFirstLetter/CapitalizeFirstLetter';
 
+import css from 'components/FeedbackOptions/FeedbackOptions.module.css' //todo = старый вариант импорта стилей
+
+
 // const CapitalizeFirstLetter = (str) => {
 //   return str[0].toUpperCase() + str.substring(1)
 // }
 
 export const FeedbackOptions = ({ options, onLeaveFeedback, good, neutral, bad, LeaveFeedback }) => (
-  <div className="FeedBack__options">
+  <div className={css.FeedBackOptions}>
     {/* <h1 className="FeedBack__title">{LeaveFeedback}</h1> */}
 
     {/* <button type="button" onClick={onLeaveFeedback}>Good</button>
@@ -17,6 +20,7 @@ export const FeedbackOptions = ({ options, onLeaveFeedback, good, neutral, bad, 
     {Object.keys(options).map(key => (
       <button key={key}
         type="button"
+        className={css.FeedBackBtn}
         onClick={onLeaveFeedback}>
         {CapitalizeFirstLetter(key)}
       </button>
